05bbf022c0e97df9c2e79d8f58d49079965a69a2
lisett liplap <lisett.liplap@vikk.ee>
2026-06-09 12:06:47 +0300

    Update hiiregajuhtmine.py

--- hiiregajuhtmine.py
+++ hiiregajuhtmine.py
@@ -1,73 +1,107 @@
-# Impordime vajalikud teegid
+# Impordib pygame teegi mänguakna ja graafika jaoks
 import pygame
+
+# Impordib random teegi juhuslike värvide tegemiseks
 import random
 
-# Käivitame pygame'i
+# Käivitab pygame'i moodulid
 pygame.init()
 
-# Määrame akna laiuse ja kõrguse
-laius, korgus = 640, 480
+# Määrab akna laiuse pikslites
+laius = 640
 
-# Loome mänguakna
+# Määrab akna kõrguse pikslites
+korgus = 480
+
+# Loob akna antud mõõtmetega
 ekraan = pygame.display.set_mode((laius, korgus))
 
-# Määrame akna pealkirja
+# Määrab akna pealkirjaks "Hiir"
 pygame.display.set_caption("Hiir")
 
-# Taustavärv (helesinine)
+# Salvestab helesinise taustavärvi RGB kujul
 TAUST = (150, 200, 245)
 
-# Loend ringide salvestamiseks
+# Loob tühja nimekirja ringide hoidmiseks
 ringid = []
 
-# Muutuja programmi tööshoidmiseks
+# Muutuja, mis hoiab programmi töös
 running = True
 
-# Peatsükkel töötab seni, kuni aken suletakse
+# Peamine mängutsükkel
 while running:
 
-    # Kontrollime kõiki sündmusi
+    # Käib läbi kõik pygame sündmused
     for event in pygame.event.get():
 
-        # Kui vajutatakse akna sulgemisnuppu
+        # Kontrollib, kas kasutaja sulges akna
         if event.type == pygame.QUIT:
+
+            # Lõpetab programmi töö
             running = False
 
-        # Kui tehakse hiireklõps
+        # Kontrollib, kas hiire nuppu vajutati
         if event.type == pygame.MOUSEBUTTONDOWN:
 
-            # Salvestame klõpsu koordinaadid
+            # Võtab hiireklõpsu x- ja y-koordinaadi
             x, y = event.pos
 
-            # Lisame uue ringi loendisse
+            # Lisab uue ringi nimekirja
             ringid.append({
-                "x": x,  # x-koordinaat
-                "y": y,  # y-koordinaat
 
-                # Juhuslik värv (boonus)
+                # Ringi x-koordinaat
+                "x": x,
+
+                # Ringi y-koordinaat
+                "y": y,
+
+                # Ringi juhuslik värv
                 "varv": (
+
+                    # Juhuslik punane väärtus
                     random.randint(0, 255),
+
+                    # Juhuslik roheline väärtus
                     random.randint(0, 255),
+
+                    # Juhuslik sinine väärtus
                     random.randint(0, 255)
                 )
             })
 
-    # Täidame tausta helesinise värviga
+            # Kontrollib, kas ringe on rohkem kui 10
+            if len(ringid) > 10:
+
+                # Kustutab kõige vanema ringi
+                ringid.pop(0)
+
+    # Värvib kogu akna taustavärviga üle
     ekraan.fill(TAUST)
 
-    # Joonistame kõik ringid ekraanile
+    # Käib läbi kõik salvestatud ringid
     for ring in ringid:
 
+        # Joonistab ringi ekraanile
         pygame.draw.circle(
-            ekraan,                     # kuhu joonistada
-            ring["varv"],               # ringi värv
-            (ring["x"], ring["y"]),     # ringi asukoht
-            10,                         # raadius 10 pikslit
-            2                           # ainult ääris, paksus 2
+
+            # Aken, kuhu joonistatakse
+            ekraan,
+
+            # Ringi värv
+            ring["varv"],
+
+            # Ringi asukoht
+            (ring["x"], ring["y"]),
+
+            # Ringi raadius
+            10,
+
+            # Ringi joone paksus
+            2
         )
 
-    # Uuendame ekraani
+    # Uuendab ekraani sisu
     pygame.display.flip()
 
-# Sulgeme pygame'i korrektselt
+# Sulgeb pygame'i korrektselt
 pygame.quit()
